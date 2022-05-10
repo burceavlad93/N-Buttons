93ad8aec764223f3be4e798545fcbc6b351360c4
Vlad Burcea <93820932+burceavlad93@users.noreply.github.com>
2022-05-10 15:03:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,35 +11,43 @@ const resetGame = document.querySelector('.reset');
 const game = document.querySelector('.buttons');
 
 document.body.addEventListener('keydown', function gameLogic(enter) {
-    console.log('key pressed');
+    // Checking if the correct key was pressed
     if (enter.key === 'Enter') {
         number = Number(userInput.value);
 
+        // Checking if the number inserted is > than 0, if not then inform the player and reveal "try again" button
         if (number <= 0) {
             tittle.textContent = "Please inser a number higher than 0";
             userInput.classList.toggle('hidden');
             tryAgain.classList.toggle('hidden');
+        // If it's > than 0, dissable keybord event, generate random number and hide input
         } else {
             document.body.removeEventListener('keydown', gameLogic);
             secretNumber = Math.trunc(Math.random() * number) + 1;
             userInput.classList.toggle('hidden');
-
+            
+            // Generatin N buttons and adding them to the HTML  
             while (number != 0) {
                 game.innerHTML += `<button class="btn"> Click! </button>`;
                 --number;
             }
-
+            
+            // Updating 'btns' object with total number of buttons
             btns = document.querySelectorAll('.btn');
 
+            // Looping the buttons to see which one was pressed 
             for (let i = 0; i < btns.length; ++i) {
                 btns[i].addEventListener('click', function () {
-
+                    
+                    // If the random number is equal to the index of the button, then change background color, change tittle, and hide all buttons
                     if (secretNumber === i) {
                         document.body.style.backgroundColor = '#60b347';
                         tittle.textContent = "You Won!"
                         game.classList.toggle('hidden');
+                    // If the index of the button is 0, don't change the background color
                     } else if (i === 0) {
                         document.body.style.backgroundColor = '#222';
+                    // If the button index is != from the hidden number then change background color, inform player, hide buttons, reveal "try again" button, resume game
                     } else {
                         document.body.style.backgroundColor = '#7c0d0d';
                         tittle.textContent = 'Wrong Choise';
@@ -48,7 +56,7 @@ document.body.addEventListener('keydown', function gameLogic(enter) {
                     }
                 })
             }
-
+            // Secret cheat hack
             tittle.addEventListener('click', function () {
                 --hiddenCheat;
                 if (hiddenCheat === 0) {
@@ -60,6 +68,7 @@ document.body.addEventListener('keydown', function gameLogic(enter) {
     }
 })
 
+// Reverts back to either input stage or game stage
 tryAgain.addEventListener('click', function () {
     document.body.style.backgroundColor = '#222';
     tittle.textContent = 'Guess The Button!';
@@ -67,5 +76,6 @@ tryAgain.addEventListener('click', function () {
     tryAgain.classList.toggle('hidden');
 })
 
+// Resets entire game
 resetGame.addEventListener('click', function () { location.reload() });
 
